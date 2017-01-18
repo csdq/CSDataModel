e04d9d2e1ca006e0d97d79b7494386a1c60d170a
csdq <shitingquan@MacBook-Pro.local>
2017-01-18 19:47:38 +0800

    change structure

--- TestApp/TestApp/ViewController.m
+++ TestApp/TestApp/ViewController.m
@@ -2,63 +2,11 @@
 //  ViewController.m
 //  TestApp
 //
-//  Created by shitingquan on 2017/1/18.
+//  Created by stq on 2017/1/18.
 //  Copyright © 2017年 CSDQ. All rights reserved.
 //
 
 #import "ViewController.h"
-#import <CSDataModel/CSDataModel.h>
-
-/**********/
-@interface Attribute : CSBaseModel
-@property (nonatomic , strong) NSString *tel;
-@property (nonatomic , strong) NSString *address;
-@end
-@implementation Attribute
-
-@end
-/**********/
-@interface Member : CSBaseModel
-@property (nonatomic , strong) NSString *name;
-@property (nonatomic , strong) Attribute *attr;
-@end
-@implementation Member
-- (instancetype)init
-{
-    self = [super init];
-    if (self) {
-        [self.subModelDict setObject:@"Attribute" forKey:@"attr"];
-    }
-    return self;
-}
-
-@end
-/**********/
-@interface Department : CSBaseModel
-@property (nonatomic , strong) NSString *name;
-@property (nonatomic , strong) NSNumber* number;
-@property (nonatomic , strong) NSArray<Department*> *sub;
-@property (nonatomic , strong) Attribute *attr;
-@property (nonatomic , strong) NSArray<Member*> *members;
-@end
-
-@implementation Department
-- (instancetype)init
-{
-    self = [super init];
-    if (self) {
-        [self.subModelDict setObject:@"Department" forKey:@"sub"];
-        [self.subModelDict setObject:@"Member" forKey:@"members"];
-        [self.subModelDict setObject:@"Attribute" forKey:@"attr"];
-    }
-    return self;
-}
-@end
-
-
-
-
-
 @interface ViewController ()
 
 @end
@@ -67,13 +15,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSDictionary *departmentInfo = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"example" withExtension:@"json"]] options:NSJSONReadingAllowFragments error:nil];
-    NSLog(@"\ndepartmentInfo Dict:\n%@",departmentInfo);
-    Department *department = [Department modelFromDict:departmentInfo];
-    NSLog(@"\ndepartmentInfo Model:\n%@",department);
-    
-    NSLog(@"\nModel to Dict : \n%@",[department modelToDict]);
-    
 }
 
 
--- TestApp/TestAppTests/TestAppTests.m
+++ TestApp/TestAppTests/TestAppTests.m
@@ -2,11 +2,58 @@
 //  TestAppTests.m
 //  TestAppTests
 //
-//  Created by shitingquan on 2017/1/18.
+//  Created by stq on 2017/1/18.
 //  Copyright © 2017年 CSDQ. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
+#import <CSDataModel/CSDataModel.h>
+
+/**********/
+@interface Attribute : CSBaseModel
+@property (nonatomic , strong) NSString *tel;
+@property (nonatomic , strong) NSString *address;
+@end
+@implementation Attribute
+
+@end
+/**********/
+@interface Member : CSBaseModel
+@property (nonatomic , strong) NSString *name;
+@property (nonatomic , strong) Attribute *attr;
+@end
+@implementation Member
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        [self.subModelDict setObject:@"Attribute" forKey:@"attr"];
+    }
+    return self;
+}
+
+@end
+/**********/
+@interface Department : CSBaseModel
+@property (nonatomic , strong) NSString *name;
+@property (nonatomic , strong) NSNumber* number;
+@property (nonatomic , strong) NSArray<Department*> *sub;
+@property (nonatomic , strong) Attribute *attr;
+@property (nonatomic , strong) NSArray<Member*> *members;
+@end
+
+@implementation Department
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        [self.subModelDict setObject:@"Department" forKey:@"sub"];
+        [self.subModelDict setObject:@"Member" forKey:@"members"];
+        [self.subModelDict setObject:@"Attribute" forKey:@"attr"];
+    }
+    return self;
+}
+@end
 
 @interface TestAppTests : XCTestCase
 
@@ -17,6 +64,14 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)testDataModelConvertion{
+    NSDictionary *departmentInfo = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"example" withExtension:@"json"]] options:NSJSONReadingAllowFragments error:nil];
+    NSLog(@"\ndepartmentInfo Dict:\n%@",departmentInfo);
+    Department *department = [Department modelFromDict:departmentInfo];
+    NSLog(@"\ndepartmentInfo Model:\n%@",department);
+    NSLog(@"\nModel to Dict : \n%@",[department modelToDict]);
 }
 
 - (void)tearDown {
